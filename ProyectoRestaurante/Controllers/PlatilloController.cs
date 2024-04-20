eec94ac310f8d3886f6e8ece857bc86efb617bb4
LuisRoj <luisrojas150520@gmail.com>
2024-04-19 23:30:44 -0500

    Agregado el Listar de Platillos o Portal de ventas, a falta de añadir mas variedad.

---
+++ ProyectoRestaurante/Controllers/PlatilloController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoRestaurante.Models;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ProyectoRestaurante.Controllers
+{
+    public class PlatilloController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public PlatilloController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await Task.Run(() => ListarPlatillos()));
+        }
+
+        public List<Platillo> ListarPlatillos()
+        {
+            List<Platillo> lista = new List<Platillo>();
+            using (SqlConnection cnn = new SqlConnection(_config["ConnectionStrings:sql"]))
+            {
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand("usp_Platillo_Listar", cnn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    lista.Add(new Platillo
+                    {
+                        id = dr.GetInt32(0),
+                        nombre = dr.GetString(1),
+                        descripcion = dr.GetString(2),
+                        precio = dr.GetDecimal(3),
+                        stock = dr.GetInt32(4),
+                        imagen = dr.GetString(5) // Asegúrate de que la ruta de la imagen se almacena en la base de datos
+                    });
+                }
+                dr.Close();
+            }
+            return lista;
+        }
+       
+    }
+}
---
+++ ProyectoRestaurante/Models/Platillo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoRestaurante.Models
+{
+    public class Platillo
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
+        public int stock { get; set; }
+        public string imagen { get; set; }
+    }
+}
