f377b37e324cf9c97a2fb902c1d5d5a34cb642fe
SP4RKS42 <i202120567@cibertec.edu.pe>
2024-04-24 18:06:15 -0500

    Listado-de-Productos-CÉSARCOAGUILAACEVEDO

---
+++ ProyectoRestaurante/Controllers/ProductoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using ProyectoRestaurante.Models;
+
+namespace ProyectoRestaurante.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly IConfiguration _config;
+        public ProductoController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        IEnumerable<Producto> Productos()
+        {
+            List<Producto> temporal = new List<Producto>();
+            using (SqlConnection cn = new SqlConnection(_config["ConnectionStrings:sql"]))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("usp_ListarProductos", cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    temporal.Add(new Producto()
+                    {
+                        Id = dr.GetInt32(0),
+                        Nombre = dr.GetString(1),
+                        Descripcion = dr.GetString(2),
+                        Precio = dr.GetDecimal(3),
+                        Stock = dr.GetInt32(4),
+
+                    });
+                }
+                dr.Close();
+            }
+            return temporal;
+        }
+        public async Task<IActionResult> Productos(int p = 0)
+        {
+            IEnumerable<Producto> temporal = Productos();
+
+            int fila = 15;
+            int c = temporal.Count();
+            int pags = c % fila == 0 ? c / fila : c / fila + 1;
+
+            ViewBag.p = p;
+            ViewBag.pags = pags;
+
+            return View(await Task.Run(() => temporal.Skip(fila * p).Take(fila)));
+        }
+    }
+}
