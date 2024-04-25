561a3d3afeaeb9d461e64d2f36c9739ca310285a
JuanCarlos1404 <juancarlosmazuelos14@gmail.com>
2024-04-25 16:52:00 -0500

    Registro de usuarios

---
+++ ProyectoRestaurante/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoRestaurante.Models
+{
+        public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
