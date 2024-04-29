443b451537796161de330d31e79d1d1f1d12293e
nilthon1234 <jhonhinostroza1234@gmail.com>
2024-04-28 19:48:24 -0500

    cambie botondel index por selecionar modifque el appsettings.json tambien la conecion de la base de datos añadi vistas carrito select tambien vistas partiales  css js etc

---
+++ ProyectoRestaurante/Models/Registro.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoRestaurante.Models
+{
+    public class Registro
+    {
+        public int id { set; get; }
+        public string nombre { get; set; }
+        public decimal precio { get; set; }
+        public int cantidad { get; set; }
+        public string Imagen { get; set; }
+        public decimal monto { get { return precio * cantidad; } }
+    }
+}
--- ProyectoRestaurante/wwwroot/js/site.js
+++ ProyectoRestaurante/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Función para verificar si hay registros en la página
+function hayRegistrosEnPagina() {
+    var contenedorRegistros = document.querySelector('.contenedor-registros');
+    return contenedorRegistros.innerHTML.trim() !== ''; 
+}
+
+
+
+
+function aplicarVibracionSiHayRegistros() {
+    var botonCarrito = document.querySelector('.carrito-btn');
+
+   
+    if (hayRegistrosEnPagina()) {
+   
+        botonCarrito.classList.add('vibrar');
+    } else {
+    
+        botonCarrito.classList.remove('vibrar');
+    }
+}
+
+
+window.onload = function () {
+    aplicarVibracionSiHayRegistros();
+};
