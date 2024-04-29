c063204e8e4bc66381c105494f9c5e516b1b8130
JuanCarlos1404 <juancarlosmazuelos14@gmail.com>
2024-04-29 17:50:36 -0500

    login y algunas actualizaciones a la clase usuario y su controlador

--- ProyectoRestaurante/Models/Usuario.cs
+++ ProyectoRestaurante/Models/Usuario.cs
@@ -1,6 +1,10 @@
-﻿namespace ProyectoRestaurante.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoRestaurante.Models
 {
-        public class Usuario
+    // Este modelo se utiliza tanto para el registro de nuevos usuarios como para el inicio de sesión.
+    // En el contexto del inicio de sesión, solo se requieren el correo electrónico y la contraseña.
+    public class Usuario
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
@@ -8,5 +12,14 @@
         public string Correo { get; set; }
         public string Telefono { get; set; }
         public string Direccion { get; set; }
+      
+        [Required(ErrorMessage = "La contraseña es requerida.")]
+        [DataType(DataType.Password)]
+        public string Contraseña { get; set; }
+
+        [Required(ErrorMessage = "Por favor, confirme la contraseña.")]
+        [DataType(DataType.Password)]
+        [Compare("Contraseña", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmarContraseña { get; set; }
     }
 }
