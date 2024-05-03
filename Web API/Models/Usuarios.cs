8d9fd23196de3d48289b6eb0b9a9e351a2718ecd
JuanCarlos1404 <juancarlosmazuelos14@gmail.com>
2024-05-03 09:27:42 -0500

    Prueba de CRUD - probar el controlador UsuariosAPI

---
+++ Web API/Models/Usuarios.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_API.Models
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida.")]
+        [DataType(DataType.Password)]
+        public string Contraseña { get; set; }
+
+        [Required(ErrorMessage = "Por favor, confirme la contraseña.")]
+        [DataType(DataType.Password)]
+        [Compare("Contraseña", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmarContraseña { get; set; }
+    }
+}
