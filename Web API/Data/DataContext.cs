e86a42bf687f1e6f1a5eec4ae232fc2e7a1ee2da
JuanCarlos1404 <juancarlosmazuelos14@gmail.com>
2024-05-03 22:17:46 -0500

    falta publicar

---
+++ Web API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Web_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuración de entidades y relaciones si es necesario
+        }
+    }
+}
--- Web API/Program.cs
+++ Web API/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DevExpress.Data.Browsing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Web_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Web_APIContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Web_APIContext") ?? throw new InvalidOperationException("Connection string 'Web_APIContext' not found.")));
@@ -9,6 +11,14 @@ builder.Services.AddDbContext<Web_APIContext>(options =>
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
