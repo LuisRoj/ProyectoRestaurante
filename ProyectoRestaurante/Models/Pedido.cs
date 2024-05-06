c234d6e4fa72179e912c65a4f891f39cbaf32aa0
SP4RKS42 <i202120567@cibertec.edu.pe>
2024-05-06 10:22:26 -0500

    Cancelar pedido terminado

--- ProyectoRestaurante/Models/Pedido.cs
+++ ProyectoRestaurante/Models/Pedido.cs
@@ -4,6 +4,9 @@ namespace ProyectoRestaurante.Models
 {
     public class Pedido
     {
+        [Display(Name = " Id Cliente")] public int id { get; set; }
+        [Display(Name = " Numero Pedido")] public string npedido { get; set; }
+        [Display(Name = " Fecha")] public DateTime fpedido { get; set; }
         [Display(Name = " DNI Cliente")] public string dnicliente { get; set; }
         [Display(Name = " Nombre Cliente")] public string nombrecliente { get; set; }
         [Display(Name = " Email Cliente")] public string emailcliente { get; set; }
