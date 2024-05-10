1b4e3fea09048c5e1216cf5ebf6aa88aaad75ee1
Juan Carlos <juancarlosmazuelos14@gmail.com>
2024-05-09 22:06:04 -0500

    CRUD

---
+++ Web API/Repositorio/Interfaces/IUsuario.cs
@@ -0,0 +1,15 @@
+﻿using Web_API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Web_API.Repositorio.Interfaces
+{
+	public class iUsuario
+	{
+		IEnumerable<Usuario> ObtenerUsuarios();
+		Usuario ObtenerUsuarioPorId(int id);
+		string InsertarUsuario(Usuario usuario);
+		string EditarUsuario(Usuario usuario);
+		void EliminarUsuario(int id);
+	}
+}
